3ab0d2d99670c6dd843a65de72597236edd6ad7d
Otávio Fernandes <ofernandes@schubergphilis.com>
2019-03-24 13:33:59 +0100

    Refactoring Handler, improving test coverage.

--- pkg/shorty/handler.go
+++ pkg/shorty/handler.go
@@ -13,91 +13,65 @@ import (
 )
 
 // Handler http endpoint handlers.
-type Handler struct{}
+type Handler struct {
+	persistence *Persistence // persistence instance
+}
 
 // Slash or root, just shows the app name.
 func (h *Handler) Slash() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("content-type", "application/json; charset=UTF-8")
-		answer, err := json.Marshal(map[string]string{"app": "shorty"})
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			h.encodeErr(w, err)
-			return
-		}
 		w.WriteHeader(http.StatusOK)
-		w.Write(answer)
+		h.writeJSON(w, map[string]string{"app": "shorty"})
 	})
 }
 
 // Create a new Shortened and store in database.
-func (h *Handler) Create(p *Persistence) http.HandlerFunc {
+func (h *Handler) Create() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var shortened *Shortened
 		var short string
-		var answer []byte
 		var err error
 
 		w.Header().Set("content-type", "application/json; charset=UTF-8")
 
-		if short, err = h.extractVar(r, "short"); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			h.encodeErr(w, err)
+		if short = h.extractShort(w, r); short == "" {
 			return
 		}
-		if shortened, err = h.readBody(r.Body); err != nil {
-			w.WriteHeader(http.StatusUnprocessableEntity)
-			h.encodeErr(w, err)
+		if shortened, err = h.extractShortened(w, r); err != nil {
 			return
 		}
-
 		shortened.Short = short
 		shortened.CreatedAt = time.Now().Unix()
 
 		log.Printf("Saving short string '%s' for URL '%s'", shortened.Short, shortened.URL)
-		if err = p.Write(shortened); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			h.encodeErr(w, err)
+		if err = h.persist(w, r, shortened); err != nil {
 			return
 		}
 
 		log.Printf("Successfully stored URL!")
 		w.WriteHeader(http.StatusCreated)
-		if answer, err = json.Marshal(shortened); err != nil {
-			h.encodeErr(w, err)
-			return
-		}
-		w.Write(answer)
+		h.writeJSON(w, shortened)
 	})
-
 }
 
 // Read long URL from database, based in short string, and execute the redirect.
-func (h *Handler) Read(p *Persistence) http.HandlerFunc {
+func (h *Handler) Read() http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var short string
 		var shortened *Shortened
 		var err error
 
-		w.Header().Set("content-type", "application/json; charset=UTF-8")
-
-		if short, err = h.extractVar(r, "short"); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			h.encodeErr(w, err)
+		if short = h.extractShort(w, r); short == "" {
 			return
 		}
 
 		log.Printf("Searching for long URL for short string '%s'", short)
-		if shortened, err = p.Read(short); err != nil {
-			if p.IsErrNoRows(err) {
-				log.Printf("No URL found for '%s' short string", short)
-				w.WriteHeader(http.StatusNoContent)
-				return
-			}
-
-			log.Printf("Error on reading persisted data: '%s'", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			h.encodeErr(w, err)
+		if shortened, err = h.retrieve(w, r, short); err != nil {
+			return
+		}
+		if shortened.URL == "" {
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 
@@ -105,35 +79,6 @@ func (h *Handler) Read(p *Persistence) http.HandlerFunc {
 		w.Header().Set("location", shortened.URL)
 		w.WriteHeader(http.StatusTemporaryRedirect)
 	})
-}
-
-// readBody capture the body and threats as a Shortened object, trying to unmarshal it.
-func (h *Handler) readBody(body io.ReadCloser) (*Shortened, error) {
-	var bodyBytes []byte
-	var err error
-
-	if bodyBytes, err = ioutil.ReadAll(body); err != nil {
-		return nil, err
-	}
-	if err = body.Close(); err != nil {
-		return nil, err
-	}
-
-	shortened := Shortened{}
-	if err = json.Unmarshal(bodyBytes, &shortened); err != nil {
-		return nil, err
-	}
-
-	return &shortened, nil
-}
-
-// extractVar extract a given variable from mux.Vars, like for instance sub-path parts.
-func (h *Handler) extractVar(r *http.Request, name string) (string, error) {
-	reqVars := mux.Vars(r)
-	if _, found := reqVars[name]; !found {
-		return "", fmt.Errorf("variable named '%s' not found in request", name)
-	}
-	return reqVars[name], nil
 }
 
 // encodeErr based in informed error it encodes the data to json and write it back as answer. To be
@@ -147,7 +92,111 @@ func (h *Handler) encodeErr(w http.ResponseWriter, err error) {
 	}
 }
 
+// extractVar extract a given variable from mux.Vars, like for instance sub-path parts.
+func (h *Handler) extractVar(r *http.Request, name string) (string, error) {
+	reqVars := mux.Vars(r)
+	if _, found := reqVars[name]; !found {
+		return "", fmt.Errorf("variable named '%s' not found in request", name)
+	}
+	return reqVars[name], nil
+}
+
+// extractShort short string used in sub-path.
+func (h *Handler) extractShort(w http.ResponseWriter, r *http.Request) string {
+	var short string
+	var err error
+
+	if short, err = h.extractVar(r, "short"); err != nil {
+		log.Printf("Error on extracting 'short': '%s'", err)
+		w.WriteHeader(http.StatusBadRequest)
+		h.encodeErr(w, err)
+		return ""
+	}
+
+	return short
+}
+
+// readBody bytes, dealing with ioutil.
+func (h *Handler) readBody(body io.ReadCloser) ([]byte, error) {
+	var bodyBytes []byte
+	var err error
+
+	if bodyBytes, err = ioutil.ReadAll(body); err != nil {
+		return nil, err
+	}
+	if err = body.Close(); err != nil {
+		return nil, err
+	}
+
+	return bodyBytes, nil
+}
+
+// extractShortened instance from request body.
+func (h *Handler) extractShortened(w http.ResponseWriter, r *http.Request) (*Shortened, error) {
+	var shortened *Shortened
+	var bodyBytes []byte
+	var err error
+
+	if bodyBytes, err = h.readBody(r.Body); err != nil {
+		log.Printf("Error on reading body bytes: '%s'", err)
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		h.encodeErr(w, err)
+		return nil, err
+	}
+
+	if err = json.Unmarshal(bodyBytes, &shortened); err != nil {
+		log.Printf("Error on marshaling bytes to Shortened instance: '%s'", err)
+		w.WriteHeader(http.StatusUnprocessableEntity)
+		h.encodeErr(w, err)
+		return nil, err
+	}
+
+	return shortened, nil
+}
+
+// retrieve shortned instance based on short string, from database.
+func (h *Handler) retrieve(w http.ResponseWriter, r *http.Request, short string) (*Shortened, error) {
+	var shortened *Shortened
+	var err error
+
+	if shortened, err = h.persistence.Read(short); err != nil {
+		if h.persistence.IsErrNoRows(err) {
+			log.Printf("No URL found for '%s' short string", short)
+			return shortened, nil
+		}
+
+		log.Printf("Error on reading persisted data: '%s'", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		h.encodeErr(w, err)
+		return nil, err
+	}
+
+	return shortened, nil
+}
+
+// persist shortened instance to database.
+func (h *Handler) persist(w http.ResponseWriter, r *http.Request, shortened *Shortened) error {
+	if err := h.persistence.Write(shortened); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		h.encodeErr(w, err)
+		return err
+	}
+	return nil
+}
+
+// writeJSON using response-writer instance to marshal any payload.
+func (h *Handler) writeJSON(w http.ResponseWriter, payload interface{}) {
+	var answer []byte
+	var err error
+
+	if answer, err = json.Marshal(payload); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		h.encodeErr(w, err)
+	}
+	w.Write(answer)
+}
+
 // NewHandler creates a new handler instance.
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(persistence *Persistence) *Handler {
+	return &Handler{persistence: persistence}
 }
--- pkg/shorty/handler_test.go
+++ pkg/shorty/handler_test.go
@@ -1,6 +1,7 @@
 package shorty
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -15,8 +16,51 @@ import (
 var handler *Handler
 
 func TestHandlerNew(t *testing.T) {
-	handler = NewHandler()
+	DeleteDatabaseFile(t)
+	p, _ := NewPersistence(&Config{DatabaseFile: databaseFile})
+
+	handler = NewHandler(p)
 	assert.NotNil(t, handler)
+}
+
+func TestHandlerEncodeErr(t *testing.T) {
+	err := fmt.Errorf("error")
+	rr := httptest.NewRecorder()
+
+	handler.encodeErr(rr, err)
+
+	assert.Equal(t, "{\"err\":{},\"msg\":\"error\"}\n", rr.Body.String())
+}
+
+func TestHandlerReadBody(t *testing.T) {
+	body := []byte("string")
+	req, err := http.NewRequest("POST", "/", bytes.NewBuffer(body))
+	assert.Nil(t, err)
+
+	b, err := handler.readBody(req.Body)
+
+	assert.Nil(t, err)
+	assert.Equal(t, body, b)
+}
+
+func TestHandlerExtractShortened(t *testing.T) {
+	body := []byte(fmt.Sprintf("{\"url\":\"%s\"}", longURL))
+	req, err := http.NewRequest("POST", "/", bytes.NewBuffer(body))
+	assert.Nil(t, err)
+
+	rr := httptest.NewRecorder()
+	shortened, err := handler.extractShortened(rr, req)
+
+	assert.Nil(t, err)
+	assert.Equal(t, longURL, shortened.URL)
+}
+
+func TestHandlerWriteJSON(t *testing.T) {
+	rr := httptest.NewRecorder()
+
+	handler.writeJSON(rr, map[string]string{"test": "test"})
+
+	assert.Equal(t, "{\"test\":\"test\"}", rr.Body.String())
 }
 
 func TestHandlerSlash(t *testing.T) {
@@ -29,16 +73,37 @@ func TestHandlerSlash(t *testing.T) {
 	h.ServeHTTP(rr, req)
 
 	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.Equal(t, "{\"app\":\"shorty\"}", rr.Body.String())
+}
+
+func TestHandlerPersist(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	assert.Nil(t, err)
+
+	shortened := &Shortened{Short: "short", URL: "URL", CreatedAt: 0}
+	rr := httptest.NewRecorder()
+
+	err = handler.persist(rr, req, shortened)
+
+	assert.Nil(t, err)
+}
+
+func TestHandlerRetrieve(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	assert.Nil(t, err)
+
+	rr := httptest.NewRecorder()
+	shortened, err := handler.retrieve(rr, req, "short")
+
+	assert.Nil(t, err)
+	assert.Equal(t, "URL", shortened.URL)
 }
 
 func TestHandlerCreate(t *testing.T) {
-	DeleteDatabaseFile(t)
-	p, _ := NewPersistence(&Config{DatabaseFile: databaseFile})
-
-	payload := strings.NewReader(fmt.Sprintf("{ \"url\": \"%s\" }", longURL))
+	payload := strings.NewReader(fmt.Sprintf("{\"url\":\"%s\"}", longURL))
 
 	r := mux.NewRouter()
-	r.HandleFunc("/{short}", handler.Create(p))
+	r.HandleFunc("/{short}", handler.Create())
 
 	ts := httptest.NewServer(r)
 	res, err := http.Post(fmt.Sprintf("%s/%s", ts.URL, short), "application/json", payload)
@@ -48,10 +113,8 @@ func TestHandlerCreate(t *testing.T) {
 }
 
 func TestHandlerRead(t *testing.T) {
-	p, _ := NewPersistence(&Config{DatabaseFile: databaseFile})
-
 	r := mux.NewRouter()
-	r.HandleFunc("/{short}", handler.Read(p))
+	r.HandleFunc("/{short}", handler.Read())
 
 	ts := httptest.NewServer(r)
 	req, _ := http.NewRequest("GET", fmt.Sprintf("%s/%s", ts.URL, short), nil)
